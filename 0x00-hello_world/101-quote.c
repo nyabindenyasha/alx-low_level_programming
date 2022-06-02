17d67692770d9ca30bc3cc97f8843ad4f9179d9d
Nyabinde Nyasha Elvis <37302304+nyabindenyasha@users.noreply.github.com>
2022-06-02 21:47:07 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fwrite(ch, sizeof(ch) - 1, 1, stdout); 
+fwrite(ch, sizeof(ch) - 1, 1, stdout);
 return (1);
 }
