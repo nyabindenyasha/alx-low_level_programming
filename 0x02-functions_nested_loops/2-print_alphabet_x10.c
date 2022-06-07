5bc33fd7278c8bc3cb014900cd5d469bfa3e19c9
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-06-07 22:10:18 +0200

    2 print alphabet_x10 correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,13 @@
 
 void print_alphabet_x10(void)
 {
-	char i;
-	int j;
+	char letter;
+	int n;
 
-	for (j = 0; j < 10; j++)
+	for (n = 0; n < 10; n++)
 	{
-		print_alphabet();
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
 }
