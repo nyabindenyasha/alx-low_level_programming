36c71f34e4941fb2850079bb883219cccbaebb59
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-06-07 23:54:37 +0200

    test

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,14 +15,14 @@ int main(void)
 	unsigned long sum_of_two = 3;
 
 	printf("1, 2, ");
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 96; i++)
 	{
 		sum_of_two = first + second;
 		printf("%ld", sum_of_two);
 		first = second;
 		second = sum_of_two;
 
-		if (i != 97)
+		if (i != 95)
 			printf(", ");
 	}
 	printf("\n");
