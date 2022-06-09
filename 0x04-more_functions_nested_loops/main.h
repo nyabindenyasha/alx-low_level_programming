c8e16206f1d3955485b83e9580e02fe0f19b56bb
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-06-09 14:53:38 +0200

    tasks 2-5

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,10 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+int _putchar();
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
 
 #endif /* MAIN_H */
