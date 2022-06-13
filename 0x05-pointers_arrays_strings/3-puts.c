15f35ae7e5d8e0c983dcdefea87ccd075855f809
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-06-13 23:09:36 +0200

    betty correction

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string, followed by a new line, 
+ * _puts - Write a function that prints a string, followed by a new line,
  * to stdout.
  * @str: an input string
  * Return: Nothing
