1f9ff637db484c5aee485d0d36c9eab769d6effb
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-06-16 11:06:40 +0200

    tasks 0 and 1 corrections

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,15 @@ char *_strcat(char *dest, char *src)
 {
 	int a = -1, dest_length = 0, src_length = 0;
 
-	while (dest[dest_length] != '\0')
+	for (; dest[dest_length] != '\0'; dest_length++)
+		;
+
+	do
+	{
+		a++;
+		dest[dest_length] = src[a];
 		dest_length++;
-
-	while (dest[src_length] != '\0')
-		src_length++;
-
-	for (int i = dest_length; i < dest_length + src_length; i++)
-		dest[i] = src[i - src_length];
+	} while (src[a] != '\0');
 
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 
 	int dest_length, i;
 
-	while (dest[dest_length] != '\0')
-		dest_length++;
-
+	for (dest_length = 0; dest[dest_length] != '\0'; dest_length++)
+		;
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_length + i] = src[i];
 
-	/*should end with a end of string char*/
 	dest[dest_length + i] = '\0';
 
 	return (dest);
