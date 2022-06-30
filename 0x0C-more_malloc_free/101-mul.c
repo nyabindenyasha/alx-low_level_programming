2d4f071eefe1452cc3a2de3166e2e2e664bd1a3f
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-07-01 01:22:38 +0200

    corr 22

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,48 @@
 #include "main.h"
-#include <stdlib.h>
-#include "3-puts.c"
-#include "100-atoi.c"
+
+/**
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
+ */
+
+int _atoi(const char *s)
+{
+	int sign = 1;
+	unsigned long int resp = 0, firstNum, i;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
+}
 
 /**
  * print_int - prints an integer.
@@ -14,9 +55,8 @@ void print_int(unsigned long int n)
 
 	unsigned long int divisor = 1, i, result;
 
-	for (i = 0; n / divisor > 9; i++, divisor *= 10){
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
 		;
-	}
 
 	for (; divisor >= 1; n %= divisor, divisor /= 10)
 	{
