df47b239bf70c29eaf81e3b6c26849972fbd3cbc
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-07-04 23:03:55 +0200

    mandatory

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#define NAME __FILE__
+
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of the file it was compiled from,
+ *  followed by a new line.
+ * Return: 0 always.
+ */
+
+int main(void)
+{
+  printf("%s\n", NAME);
+  return (0);
+}
