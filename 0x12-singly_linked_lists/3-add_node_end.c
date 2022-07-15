8628e2d8bfe1eae11e617a809e5c3b36e0e86c57
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-07-15 02:12:00 +0200

    corrections

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include "lists.h"
-#include "2-strlen.c"
 
 /**
-  * add_node_end - a function that adds a new node at the end of a list.
-  * @head: Linked list.
-  * @str: The string.
-  * Return: The new node.
-  */
+ * _strlen - Return the len of a string.
+ * @s: The string.
+ * Return: Lenght of a string.
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node_end - a function that adds a new node at the end of a list.
+ * @head: Linked list.
+ * @str: The string.
+ * Return: The new node.
+ */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
