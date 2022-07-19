d4b1d6a3803675bd46350e4613862bc469fc4ea5
nyabindenyasha@gmail.com <nyabindenyasha@gmail.com>
2022-07-19 14:18:36 +0200

    betty corrections

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 
 /**
-  * free_listint2 - a function that frees a listint_t list.
-  * @head: The list.
-  * Return: void.
-  */
+ * free_listint2 - a function that frees a listint_t list.
+ * @head: The list.
+ * Return: void.
+ */
 
 void free_listint2(listint_t **head)
 {
 	listint_t *current_node;
 
-	if (head == NULL){
+	if (head == NULL)
+	{
 		return;
 	}
 
